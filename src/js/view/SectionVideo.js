42d3a24f4eb9e75c625f946efa31a28b1ce8f786
Patrick Matte <patrickmatte@gmail.com>
2024-07-14 19:39:32 -0400

    cleanup

--- src/js/view/SectionVideo.js
+++ src/js/view/SectionVideo.js
@@ -8,19 +8,12 @@ export default class SectionVideo extends Section {
   constructor(element) {
     super(element);
 
-    // this.iframe = this.element.querySelector('iframe');
-    // this.iframe.src = chrome.runtime.getURL('video-recording.html');
-
     chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
       switch (msg.type) {
-        // case 'scrollCaptureVideoHeight':
-        //   this.iframe.style.height = msg.height + 'px';
-        //   break;
         case 'scrollCaptureVideoURL':
           this.updateVideo(msg);
           break;
         case 'scrollCaptureFFmpegLogToCC':
-          // console.log('sc msg', msg);
           app.model.ffmpegLogs.push(msg.message);
           sendResponse({ gotIt: true });
           break;
@@ -34,7 +27,6 @@ export default class SectionVideo extends Section {
     player.setAttribute('autoplay', 'true');
     player.setAttribute('playsinline', 'true');
     player.setAttribute('controls', '1');
-    // player.setAttribute('controlsList', 'nodownload');
     player.addEventListener('canplay', () => {
       this.showVideo();
     });
@@ -92,12 +84,10 @@ export default class SectionVideo extends Section {
     app.model.ffmpegLogs.value = [];
     let promise = super.showDelayComplete();
     app.model.sendMessage({ type: 'scrollCaptureShowVideo' });
-    // this.querySelector('.sc-logger-container').style.paddingTop = `${(window.innerHeight / window.innerWidth) * 100}%`;
     return promise;
   }
 
   hideComplete() {
-    // app.model.sendMessage({ type: 'scrollCaptureUnloadVideo' });
     let player = this.querySelector('.sc-video-player');
     player.pause();
     player.removeAttribute('src');
